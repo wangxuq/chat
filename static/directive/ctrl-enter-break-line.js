841e0d7073a1e8a82897143ddbe602f22ab5c4c2
wangxuq <1228845897@qq.com>
2015-03-07 23:38:17 +0800

    fix the ctrlenter problem

--- static/directive/ctrl-enter-break-line.js
+++ static/directive/ctrl-enter-break-line.js
@@ -1,19 +1,19 @@
 angular.module('techNodeApp').directive('ctrlEnterBreakLine',function(){
-    return function($scope,$element,$attrs){
+    return function(scope,element,attrs){
         var ctrlDown = false;
-        $element.bind('keydown',function(evt){
-            if(evt.which === 17){
+        element.bind('keydown',function(evt){
+            if(evt.keyCode == 17){
                 ctrlDown = true;
                 setTimeout(function(){
                     ctrlDown = false;
                 },1000);
             }
-            if(evt.which === 13){
+            if(evt.keyCode == 13){
                 if(ctrlDown){
-                    $element.val($element.val() + '\n')
+                    element.val(element.val() + '\n')
                 }else{
-                    $scope.$apply(function(){
-                       $scope.$eval($attrs.ctrlEnterBreakLine);
+                    scope.$apply(function(){
+                       scope.$eval(attrs.ctrlEnterBreakLine);
                     });
                     evt.preventDefault();
                 }
