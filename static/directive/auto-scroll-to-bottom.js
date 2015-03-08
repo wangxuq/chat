13d818867a88844be22c746c59abb501d5a4e0d2
wangxuq <1228845897@qq.com>
2015-03-08 23:22:56 +0800

    redesign the structure

--- static/directive/auto-scroll-to-bottom.js
+++ static/directive/auto-scroll-to-bottom.js
@@ -1,13 +1,13 @@
 angular.module('techNodeApp').directive('autoScrollToBottom',function(){
     return {
-        link : function($scope,$element,$attrs){
-            $scope.$watch(
+        link : function(scope,element,attrs){
+            scope.$watch(
                 function(){
-                    return $element.children().length;
+                    return element.children().length;
                 },
                 function(){
-                    $element.animate({
-                        scrollTop : $element.prop('scrollHeight')
+                    element.animate({
+                        scrollTop : element.prop('scrollHeight');
                     },1000);
                 }
             );
