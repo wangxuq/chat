7872afc51cde48e06b0bb95d699a1d40401aa3cb
wangxuxa <1228845897@qq.com>
2015-03-10 11:11:10 +0800

    add the login function 3

--- app.js
+++ app.js
@@ -2,13 +2,60 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var path = require('path');
-var Controllers = require('./Controllers');
+var Controllers = require('./controllers');
 
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({
+    secret : chat,
+    cookie : {
+        maxAge : 60*1000
+    }
+}));
 
 app.use(express.static(__dirname+'/static'));
 app.use(function(req,res){
     res.sendFile(path.join(__dirname, './static', 'index.html'));
 });
+
+//the interface of login validate
+app.get('/api/validate',function(req,res){
+    _userId = req.session._userId;
+    if(_userId){
+        Controllers.User.findUserById(_userId,function(err,callback){
+            if(err){
+                res.json(401,{msg : err});
+            }else{
+                res.json(user);
+            }
+        })
+    }else{
+        res.json(401,null);
+    }
+});
+app.post('/api/login',function(req,res){
+    email = req.body.email;
+    if(email){
+        Controllers.User.findByEmailOrCreate(email,function(err,user){
+            if(err){
+                res.json(500,{msg : err});
+            }else{
+                req.session._userId = user._id;
+                res.json(user);
+            }
+        })
+    }else{
+        res.json(401);
+    }
+});
+//logout validate
+app.get('/api/logout',function(req,res){
+    req.session._userId = null;
+    res.json(401);
+});
+
+
+
 
 var io = require('socket.io').listen(app.listen(port));
 var messages = [];
--- static/technode.js
+++ static/technode.js
@@ -8,6 +8,18 @@ run(function($window,$rootScope,$http,$location){
         $rootScope.me = user;
         $location.path('/');
     }).error(function(data){
-        %location.path('/login');
-    })
+        $location.path('/login');
+    });
+    $rootScope.logout=function(){
+        $http({
+            url : '/ajax/logout',
+            method : 'GET'
+        }).success(function(){
+            $rootScope.me = null;
+            $location.path('/login');
+        });
+    };
+    $rootScope.$on('login',function(evt,me){
+        $rootScope.me = me;
+    });
 });
